a03ff621fffb2aca11c687b0e04b1f1c686fafbf
chrisscholl16 <135916179+chrisscholl16@users.noreply.github.com>
2026-09-01 18:40:32 -0400

    change to index

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def admin_required(f):
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
